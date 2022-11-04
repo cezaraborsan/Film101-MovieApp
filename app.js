0c0a3b8dabd5a78977305f09096a095e496dadd0
Cezara Borsan <cezarabo@gmail.com>
2022-11-04 21:54:50 +0200

    started movie app

---
+++ app.js
@@ -0,0 +1,219 @@
+const MY_API_KEY = "494c8f2f1b18f570d713941a3eec2c7b";
+
+const popularMovies_URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${MY_API_KEY}&page=1`;
+
+const popularTV_URL = `https://api.themoviedb.org/3/trending/tv/week?api_key=${MY_API_KEY}&page=1`;
+
+const upcomingMovies_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${MY_API_KEY}&language=en-US&page=1`;
+const SERACH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${MY_API_KEY}&query="`;
+
+const topRatedMovies_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${MY_API_KEY}&language=en-US&page=1`;
+const topRatedTV_URL = `https://api.themoviedb.org/3/tv/top_rated?api_key=${MY_API_KEY}&language=en-US&page=1`;
+const posterPath = "https://image.tmdb.org/t/p/w1280";
+
+const form = document.querySelector(".form");
+const search = document.querySelector(".search");
+const popularMoviesContainer = document.querySelector(".popular-movies");
+const searchedMoviesContainer = document.querySelector(".searched-movies");
+const upcomingMoviesContainer = document.querySelector(".upcoming-movies");
+const popularTVContainer = document.querySelector(".popular-tv");
+const standardMovies = document.querySelector(".standard-movies");
+const searchedMovies = document.querySelector(".searched");
+
+//Popular Movies
+
+getPopularMovies(popularMovies_URL);
+getUpcomingMovies(upcomingMovies_URL);
+getPopularTV(popularTV_URL);
+getTopRatedMovies(topRatedMovies_URL);
+getTopRatedTV(topRatedTV_URL);
+
+async function getPopularMovies() {
+  const response = await fetch(popularMovies_URL);
+  const data = await response.json();
+  const results = data.results.slice(0, 12);
+
+  popularMoviesContainer.innerHTML = "";
+
+  results.forEach((movie) => {
+    const { title, poster_path, release_date } = movie;
+    const movieRating = movie.vote_average.toFixed(1);
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+    <img class="poster" src="${posterPath + poster_path}" />
+    <div class="movie-details">
+      <h3 class="movie-title">${title}</h3>
+      <div class="rating">
+        <img src="../imgs/star.png" alt="rating star" />
+        <span>${movieRating}</span>
+      </div>
+    </div>
+    <p class="release">
+        Realease date:
+        <span>${release_date}</span>
+    </p>`;
+
+    popularMoviesContainer.appendChild(movieEl);
+  });
+}
+
+// Popular TV shows
+
+async function getPopularTV() {
+  const response = await fetch(popularTV_URL);
+  const data = await response.json();
+  const results = data.results.slice(0, 12);
+
+  popularTVContainer.innerHTML = "";
+
+  results.forEach((movie) => {
+    const { name, poster_path, first_air_date } = movie;
+    const movieRating = movie.vote_average.toFixed(1);
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+    <img class="poster" src="${posterPath + poster_path}" />
+    <div class="movie-details">
+      <h3 class="movie-title">${name}</h3>
+      <div class="rating">
+        <img src="../imgs/star.png" alt="rating star" />
+        <span>${movieRating}</span>
+      </div>
+    </div>
+    <p class="release">
+        First air date:
+        <span>${first_air_date}</span>
+    </p>`;
+
+    popularTVContainer.appendChild(movieEl);
+  });
+}
+
+// Coming up in theaters
+
+async function getUpcomingMovies() {
+  const response = await fetch(upcomingMovies_URL);
+  const data = await response.json();
+  const results = data.results.slice(0, 12);
+
+  upcomingMoviesContainer.innerHTML = "";
+
+  results.forEach((movie) => {
+    const { title, poster_path, vote_average, release_date } = movie;
+    const movieRating = movie.vote_average.toFixed(1);
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+    <img class="poster" src="${posterPath + poster_path}" />
+    <div class="movie-details">
+      <h3 class="movie-title">${title}</h3>
+      <div class="rating">
+        <img src="../imgs/star.png" alt="rating star" />
+        <span>${movieRating}</span>
+      </div>
+    </div>
+    <p class="release">
+    Release date:
+        <span>${release_date}</span>  
+    </p>`;
+
+    upcomingMoviesContainer.appendChild(movieEl);
+  });
+}
+
+async function getMoviesBySearch(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results.slice(0, 21);
+
+  searchedMoviesContainer.innerHTML = "";
+
+  results.forEach((movie) => {
+    const { title, poster_path, vote_average, release_date } = movie;
+    const movieRating = movie.vote_average.toFixed(1);
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+    <img class="poster" src="${posterPath + poster_path}" />
+    <div class="movie-details">
+      <h3 class="movie-title">${title}</h3>
+      <div class="rating">
+        <img src="../imgs/star.png" alt="rating star" />
+        <span>${movieRating}</span>
+      </div>
+    </div>
+    <p class="release">
+        Realease date:
+        <span>${release_date}</span>
+    </p>`;
+
+    searchedMoviesContainer.appendChild(movieEl);
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    standardMovies.style.display = "none";
+    searchedMovies.style.display = "block";
+    getMoviesBySearch(SERACH_URL + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+const topRatedMoviesList = document.querySelector(".top-rated-movies");
+const topRatedTVList = document.querySelector(".top-rated-tv-shows");
+
+async function getTopRatedMovies() {
+  const response = await fetch(topRatedMovies_URL);
+  const data = await response.json();
+  const results = data.results.slice(0, 10);
+
+  topRatedMoviesList.innerHTML = "";
+
+  results.forEach((movie) => {
+    const { title, vote_average, release_date } = movie;
+    const movieRating = movie.vote_average.toFixed(1);
+
+    const movieEl = document.createElement("li");
+    movieEl.classList.add("top-rated-item");
+    movieEl.innerHTML = `<span class="top-rated-info">
+    ${title}<span class="score">${vote_average}</span>
+  </span>
+  <span class="top-rated-release-date">${release_date}</span>`;
+
+    topRatedMoviesList.appendChild(movieEl);
+  });
+}
+
+async function getTopRatedTV() {
+  const response = await fetch(topRatedTV_URL);
+  const data = await response.json();
+  const results = data.results.slice(0, 10);
+
+  topRatedTVList.innerHTML = "";
+
+  results.forEach((movie) => {
+    const { name, overview, vote_average, first_air_date } = movie;
+    const movieRating = movie.vote_average.toFixed(1);
+
+    const movieEl = document.createElement("li");
+    movieEl.classList.add("top-rated-item");
+    movieEl.innerHTML = `
+    <span class="top-rated-info"> ${name}<span class="score">${vote_average}</span></span>
+  <span class="top-rated-release-date">${first_air_date}</span>`;
+
+    topRatedTVList.appendChild(movieEl);
+  });
+}
